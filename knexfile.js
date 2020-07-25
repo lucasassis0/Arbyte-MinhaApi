8f70617863b87b783aa42d2ee7988b52452e25f1
Lucas Assis <lucas.assis_@hotmail.com>
2020-07-24 20:01:20 -0400

    bug fix

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,19 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true,
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
     pool: {
       min: 2,
       max: 10
@@ -16,11 +28,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
